c306fdc98fa76847a787e079e0467e6d4b44906d
sophiemie <smiessner@stud.hs-bremen.de>
2025-02-24 14:49:02 +0100

    Pfadanpassung für Swagger, ungetestet

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,8 @@ import lastfmRoutes from "./routes/lastfmRoutes.js";
 import swaggerUi from "swagger-ui-express";
 import yamljs from "yamljs";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
@@ -12,14 +14,18 @@ const PORT = 4000;
 
 app.use(cors()); // CORS für alle Anfragen aktivieren
 
-// Swagger laden
-//const swaggerDocument = yamljs.load("src/docs/swagger.yaml"); // Für Docker
-const swaggerDocument = yamljs.load("docs/swagger.yaml"); // Lokal
+// __dirname in ES-Modulen definieren
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Prüfen, ob das Skript in Docker läuft (WORKDIR ist /data)
+const isDocker = process.cwd() === "/data";
+const swaggerPath = isDocker ? "/data/src/docs/swagger.yaml" : path.join(__dirname, "docs/swagger.yaml");
+const swaggerDocument = yamljs.load(swaggerPath);
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Routen einbinden
 app.use("/api", lastfmRoutes);
-
-
 
 app.listen(PORT, () => console.log(`Server läuft auf http://localhost:${PORT}`));
